bb43b5c5eb46d0a1fd9bcd7431c8379fdc2c6a67
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-02-26 01:40:54 +0600

    commit -a Localstorage

--- HomeWork_JavaScript_ToDoList/script.js
+++ HomeWork_JavaScript_ToDoList/script.js
@@ -1,40 +1,23 @@
-// document.forms.toDoForm.addEventListener('submit', () => {
-//     event.preventDefault();
-
-//     let item = document.createElement('li');
-//     item.innerText = document.forms.toDoForm.title.value;
-
-//     // item.addEventListener('click', () => {
-//     //     event.target.classList.add('done');
-//     // })
-
-//     document.querySelector('#tasks').append(item);
-
-//       //document.forms.toDoForm.title.value = '';
-//     document.forms.toDoForm.reset();
-// });
-
-
-
-// for (const li of document.querySelectorAll('#tasks > li')) {
-//     li.addEventListener('click', () => {
-//         event.target.classList.add('done');
-//     })
-// }
-
-//////////////////////////////////////////////////////
-
+let array = [];
 
 document.forms.toDoForm.addEventListener('submit', () => {
     event.preventDefault();
 
-    let item = document.createElement('li');
-    item.innerHTML = document.forms.toDoForm.title.value  + "<span>❌</span>" + ' <input id="date" type="date" value="date">';
-    document.getElementById('tasks').append(item);
-
-    //  document.forms.toDoForm.title.value = '';
+    
+    let text = document.forms.toDoForm.title.value;
+    let obj = {title: text, id: Date.now()};
+    array.push(obj);
+    drawItem(obj);
+    localStorage.setItem('array', JSON.stringify(array));
     document.forms.toDoForm.reset();
 });
+
+function drawItem(obj) {
+    let item = document.createElement('li');
+    item.innerHTML = obj.title  + "<span>❌</span>" + ' <input id="date" type="date" value="">';
+    document.getElementById('tasks').append(item);
+    
+}
 
 
 
@@ -68,3 +51,103 @@ function clearList() {
 }
 
 clear.addEventListener("click", clearList, false);
+
+document.addEventListener('DOMContentLoaded', () => {
+let part = document.cookie.split(';')[0];
+let style = part.split('=')[1]
+console.log(style);
+document.querySelector('body').style.backgroundColor = style;
+document.querySelector('h1').style.backgroundColor = style;
+document.querySelector('h1').style.color = style;
+document.querySelector('button').style.backgroundColor = style;
+document.querySelector('button').style.color = style;
+document.querySelector('#btnClear').style.backgroundColor = style;
+document.querySelector('#btnClear').style.color = style;
+});
+
+document
+.querySelector('#btn1')
+.addEventListener('click', () => {
+document.cookie = "style = Style1;max-age=3600";
+document.querySelector('body').style.backgroundColor = 'darkgray';
+document.querySelector('h1').style.backgroundColor = 'gray';
+document.querySelector('h1').style.color = 'black';
+document.querySelector('button').style.backgroundColor = 'green';
+document.querySelector('button').style.color = 'black';
+document.querySelector('#btnClear').style.backgroundColor = 'red';
+document.querySelector('#btnClear').style.color = 'black';
+});
+
+document
+.querySelector('#btn2')
+.addEventListener('click', () => {
+document.cookie = "style = Style2;max-age=3600";
+document.querySelector('body').style.backgroundColor = 'aqua';
+document.querySelector('h1').style.backgroundColor = '#4fd77f';
+document.querySelector('h1').style.color = 'darkblue';
+document.querySelector('button').style.backgroundColor = '#4fd77f';
+document.querySelector('button').style.color = 'darkblue';
+document.querySelector('#btnClear').style.backgroundColor = '#cf222e';
+document.querySelector('#btnClear').style.color = 'darkblue';
+});
+
+document
+.querySelector('#btn3')
+.addEventListener('click', () => {
+document.cookie = "style = Style3;max-age=3600";
+document.querySelector('body').style.backgroundColor = 'antiquewhite';
+document.querySelector('h1').style.backgroundColor = '#dac5b0';
+document.querySelector('h1').style.color = '#282929';
+document.querySelector('button').style.backgroundColor = '#fed55f';
+document.querySelector('button').style.color = '#282929';
+document.querySelector('#btnClear').style.backgroundColor = 'orange';
+document.querySelector('#btnClear').style.color = '#282929';
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+let style = localStorage.getItem('class');
+console.log(style);
+document.querySelector('body').classList.add(style);
+let data = localStorage.getItem('array');
+
+if (data) {
+     array = JSON.parse(data);
+
+    for (const item of array) {
+        drawItem(item);
+    }
+}
+});
+
+document
+.querySelector('#btn1')
+.addEventListener('click', () => {
+    localStorage.setItem('class', document.querySelector('body').classList.add('darkGrayTheme'));
+    
+});
+
+document
+.querySelector('#btn2')
+.addEventListener('click', () => {
+    localStorage.setItem('class', document.querySelector('body').classList.add('aquaTheme'));
+    
+// document.querySelector('h1').style.backgroundColor = '#4fd77f';
+// document.querySelector('h1').style.color = 'darkblue';
+// document.querySelector('button').style.backgroundColor = '#4fd77f';
+// document.querySelector('button').style.color = 'darkblue';
+// document.querySelector('#btnClear').style.backgroundColor = '#cf222e';
+// document.querySelector('#btnClear').style.color = 'darkblue';
+});
+
+document
+.querySelector('#btn3')
+.addEventListener('click', () => {
+    localStorage.setItem('class', document.querySelector('body').classList.add('antiquewhiteTheme'));
+    
+// document.querySelector('h1').style.backgroundColor = '#dac5b0';
+// document.querySelector('h1').style.color = '#282929';
+// document.querySelector('button').style.backgroundColor = '#fed55f';
+// document.querySelector('button').style.color = '#282929';
+// document.querySelector('#btnClear').style.backgroundColor = 'orange';
+// document.querySelector('#btnClear').style.color = '#282929';
+});
